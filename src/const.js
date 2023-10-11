0283dcb22f2850d9b51f51725a2ca720cebc4f6d
Maksim Strikanov <59795160+MaxStrikanov@users.noreply.github.com>
2023-10-11 09:15:53 +0300

    Delete src directory

--- src/const.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = 'https://workable-plucky-huckleberry.glitch.me/';
-// export const API_URL = 'http://localhost:8024/';
-export const GOODS_URL = `${API_URL}api/goods`;
-export const CATEGORY_URL = `${API_URL}api/categories`;
-export const COLORS_URL = `${API_URL}api/colors`;
-export const ORDER_URL = `${API_URL}api/order`;
